1fb079396ebf92099af1101dc74f0ff9ce12073f
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2022-09-27 15:02:55 +0200

    Create Role in asp net core

---
+++ Identity/Controllers/AdminsitrationController.cs
@@ -0,0 +1,40 @@
+﻿using Identity.DAL.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Identity.Controllers {
+    public class AdminsitrationController:Controller{
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdminsitrationController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        [HttpGet]
+        public ActionResult Create() { 
+          return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateRoleVM model) {
+            if (ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole()
+                {
+                    Name = model.RoleName
+                };
+                IdentityResult result = await _roleManager.CreateAsync(identityRole);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction(nameof(Ok), "Account");
+                }
+                foreach (IdentityError item in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, item.Description);
+                }
+                
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Identity/DAL/ViewModel/CreateRoleVM.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.DAL.ViewModel {
+    public class CreateRoleVM {
+        [Required(ErrorMessage ="Required Role Name !!")]
+        public string RoleName { get; set; }
+    }
+}
